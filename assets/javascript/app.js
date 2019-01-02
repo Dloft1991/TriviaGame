3548bde64c474414d727ab6d3f24b5aa1478682f
Dloft <derek.lofton1@gmail.com>
2019-01-02 15:19:16 -0500

    working

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,69 +1,38 @@
 var game = document.querySelector('main');
 
 
-var score = 0;
-var answer = [];
-var correct = 0;
-var wrong = 0;
-
-//questions
-var question = ["What color is Thanos?", "Who is Starloards favorite actor?", "Where is SpiderMan from?", "How many kids does HawkEye have?"];
-// var question = [
-//     {
-//         one: {
-//         ask: "What color is Thanos?",
-//         option: ["pink", "blue", "red", "purple"],
-//         right: "purple",
-//         }
-//     },
-//     {
-//         two: {
-//         ask: "Who is Starloards favorite actor?",
-//         option: ["Brian Cranston", "Samuel L. Jackson", "Kevin Bacon", "Tom Cruse"],
-//         right: "Kevin Bacon",
-//         }
-//     },
-//     {
-//         three: {
-//         ask: "Where is SpiderMan from?",
-//         option: ["Orlando", "Queens", "Brooklyn", "Portland"],
-//         right: "Queens",
-//         }
-//     },
-//     {
-//         four: {
-//         ask: "How many kids does HawkEye have?",
-//         option: ["Nine", "Three", "Two", "Five"],
-//         right: "Two",
-//         }
-//     },
-// ];
-
-console.log(question);
-console.log(question.length);
-console.log(question[0]);
-
-// var start = $("#start").on('click', function() {
-//     score = 0;
-//     wrong = 0;
-//     correct = 0;
-//     answer = [];
-// });
-// var ask = question[Math.floor(Math.random() * question.length)];
-
-for(i = 0; i < question.length; i++) {
-    ask = document.getElementById("showQuestion " + question[i]);
-    $("#showQuestion").text(question[i]);
-
-    
+function pageLoad() {
+    document.getElementById("after_start").style.visibility = "visible";
 }
 
+function check() {
 
+  var question1 = document.quiz.question1.value;
+  var question2 = document.quiz.question2.value;
+  var question3 = document.quiz.question3.value;
+  var question4 = document.quiz.question4.value;
+  var correct = 0;
+
+  if (question1 == "Purple") {
+    correct++;
+  }
+  if (question2 == "Kevin Bacon") {
+    correct++;
+  }
+  if (question3 == "Queens") {
+    correct++;
+  }
+  if (question4 == "Two") {
+    correct++;
+  }
+  // document.getElementById("after_submit").style.visibilty = "visible";
+  document.getElementById("number_correct").innerHTML = "You got " + correct + " correct.";
+}
 
 
 // timer// 
 
-var number = 10;
+var number = 11;
 
     var intervalId;
 
@@ -83,7 +52,7 @@ var number = 10;
       $("#counter").html("<h2>" + number + "</h2>");
 
       if (number === 0) {
-
+        document.getElementById("after_start").style.visibility = "hidden";
         stop();
 
         // alert("Time Up!");   
@@ -96,4 +65,4 @@ var number = 10;
     }
 
     run();
-
+  
