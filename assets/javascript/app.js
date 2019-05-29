8bc81aed71ec1b9aa84829a0aaad75e029c0f11d
Dloft <derek.lofton1@gmail.com>
2019-05-29 08:58:04 -0400

    newgmame

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,44 +1,48 @@
 var game = document.querySelector('main');
 
+window.onload = function () {
+    document.getElementById('quiz').style.visibility = 'hidden';
+    document.getElementById('submitbtn').style.visibility = 'hidden';
+    document.getElementById('resetbtn').style.visibility = 'hidden';
+};
 
-function check() {
-
-  var question1 = document.quiz.question1.value;
-  var question2 = document.quiz.question2.value;
-  var question3 = document.quiz.question3.value;
-  var question4 = document.quiz.question4.value;
-  var correct = 0;
-
-  if (question1 == "Purple") {
-    correct++;
-  }
-  if (question2 == "Kevin Bacon") {
-    correct++;
-  }
-  if (question3 == "Queens") {
-    correct++;
-  }
-  if (question4 == "Two") {
-    correct++;
-  }
-  
-
-  document.getElementById("number_correct").innerHTML = "You got " + correct + " correct.";
-  document.getElementById("after_start").style.visibility = "visible";
-  // thor image
-  // document.getElementById("#thor").style.visibility = "visible";
- 
-  
+function start() {
+    document.getElementById("quiz").style.visibility = "visible";
+    document.getElementById('submitbtn').style.visibility = 'visible';
+    document.getElementById('resetbtn').style.visibility = 'visible';
 }
 
 function restart() {
-      
-  document.getElementById("after_start").style.visibility = "hidden";
-  
-  
-  stop();
-   number = 21;
+    document.getElementById("quiz").style.visibility = "hidden";
+    // document.getElementById("number_correct").style.visibility = "hidden";
+    
+}
 
+function check() {
+console.log("working");
+    var question1 = document.quiz.question1.value;
+    var question2 = document.quiz.question2.value;
+    var question3 = document.quiz.question3.value;
+    var question4 = document.quiz.question4.value;
+    var correct = 0;
+
+    if (question1 == "Purple") {
+        correct++;
+    }
+    if (question2 == "Kevin Bacon") {
+        correct++;
+    }
+    if (question3 == "Queens") {
+        correct++;
+    }
+    if (question4 == "Two") {
+        correct++;
+    }
+
+    document.getElementById("quiz").style.visibility = "hidden";
+    document.getElementById("number_correct").innerHTML = "You got " + correct + " correct.";
+    
+    document.getElementById("after_submit").style.visibility = "visible";
 }
 
 // timer// 
